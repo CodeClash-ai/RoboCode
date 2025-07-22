410c51154d69d2f3e1932d7570ce9b3319dd3fef
John Yang <byjohnyang@gmail.com>
2025-07-22 15:57:59 -0700

    Add directory for custom robot

---
+++ robots/custom/MyTank.java
@@ -0,0 +1,17 @@
+package custom;
+import robocode.*;
+
+public class MyTank extends Robot {
+    public void run() {
+        while(true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
