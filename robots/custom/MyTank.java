11cc3dce550eb9eefad1a6a6ec6044e0814b22b4
CodeClash <player@codeclash.com>
2026-07-06 18:16:40 -0400

    Import markiv (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,91 @@
+// CodeClash ladder import
+// Source: https://github.com/Tibola/robocode/blob/HEAD/miv/MarkIV.java
+// Author: Tibola   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
+public class MyTank extends Robot
+{
+  int turnDirection = 1, ahead = 30;
+	double vidaInicialInimigo = 100;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+	public void run() {
+	  setAdjustRadarForRobotTurn(true);
+	  turnGunRight(-90);
 
-import java.awt.*;
+	  setBodyColor(new java.awt.Color(54,165,67));
+	  setGunColor(new java.awt.Color(150,150,150));
+	  setRadarColor(new java.awt.Color(102,165,104));
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+	  while(true) {
+        turnRadarRight(360);
+		if(bordas()){
+			voltarArea();
+		}
+	  }
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+      System.out.println(e.getName());
+      if (!(e.getName().equals("samplesentry.BorderGuard"))){
+        double inimigo = e.getBearing() % 360, energiaInimigo = e.getEnergy();
+
+        turnRight(inimigo+90);
+
+        if(getEnergy() > 50 && e.getDistance() < 50) {
+          fire(3);
         }
-    }
-
-    public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
-    }
+
+        if(vidaInicialInimigo != energiaInimigo){
+          ahead(50);
+        }
+
+        vidaInicialInimigo = e.getEnergy();
+      }
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		if (!(e.getName().equals("samplesentry.BorderGuard"))){
+			ahead(75);
+		}
+	}
+
+  	public void onHitWall(HitWallEvent e) {
+    	turnLeft(180 - Math.abs(e.getBearing()));
+  	}
+
+	public boolean bordas() {
+		if(getX() > (getBattleFieldWidth() - getSentryBorderSize()) || getX() < getSentryBorderSize() ){
+			return true;
+		}
+		else {
+			if(getY() > (getBattleFieldHeight() - getSentryBorderSize()) || getY() < getSentryBorderSize() ){
+				return true;
+			}
+			return false;
+		}
+	}
+
+	public void voltarArea(){
+		if (getX() > (getBattleFieldWidth() - getSentryBorderSize())) {
+			turnLeft(getHeading() + 90);
+			ahead(150);
+		}
+
+		if (getY() > (getBattleFieldWidth() - getSentryBorderSize())) {
+			turnLeft(getHeading() + 180);
+			ahead(150);
+		}
+
+		if (getX() < getSentryBorderSize()) {
+			turnRight(getHeading() + 180);
+			ahead(150);
+		}
+
+		if (getY() < getSentryBorderSize()) {
+			turnRight(getHeading() + 180);
+			ahead(150);
+		}
+	}
 }
