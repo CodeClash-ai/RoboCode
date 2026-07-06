15229c1b812bb224346a330461d6c976db3d265e
CodeClash <player@codeclash.com>
2026-07-06 18:17:12 -0400

    Import sittingduck (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,101 @@
+// CodeClash ladder import
+// Source: https://github.com/robo-code/robocode/blob/HEAD/robocode.samples/src/main/java/sample/SittingDuck.java
+// Author: Robocode (Mathew Nelson / Flemming N. Larsen et al.)   License: EPL-1.0
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
+/*
+ * Copyright (c) 2001-2025 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 package custom;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+
+import robocode.AdvancedRobot;
+import robocode.RobocodeFileOutputStream;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+/**
+ * SittingDuck - a sample robot that demonstrates file persistence.
+ * <p>
+ * This robot sits still without moving and demonstrates how to maintain persistent data
+ * across rounds and battles by reading and writing to a data file.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ * @author Andrew Magargle (contributor)
+ */
+public class MyTank extends AdvancedRobot {
+	static boolean incrementedBattles = false;
+
+	public void run() {
+		setBodyColor(Color.yellow);
+		setGunColor(Color.yellow);
+
+		int roundCount, battleCount;
+
+		try {
+			BufferedReader reader = null;
+			try {
+				// Read file "count.dat" which contains 2 lines, a round count, and a battle count
+				reader = new BufferedReader(new FileReader(getDataFile("count.dat")));
+
+				// Try to get the counts
+				roundCount = Integer.parseInt(reader.readLine());
+				battleCount = Integer.parseInt(reader.readLine());
+
+			} finally {
+				if (reader != null) {
+					reader.close();
+				}
+			}
+		} catch (IOException e) {
+			// Something went wrong reading the file, reset to 0.
+			roundCount = 0;
+			battleCount = 0;
+		} catch (NumberFormatException e) {
+			// Something went wrong converting to ints, reset to 0
+			roundCount = 0;
+			battleCount = 0;
+		}
+
+		// Increment the # of rounds
+		roundCount++;
+
+		// If we haven't incremented # of battles already,
+		// Note: Because robots are only instantiated once per battle, member variables remain valid throughout it.
+		if (!incrementedBattles) {
+			// Increment # of battles
+			battleCount++;
+			incrementedBattles = true;
+		}
+
+		PrintStream w = null;
+		try {
+			w = new PrintStream(new RobocodeFileOutputStream(getDataFile("count.dat")));
+
+			w.println(roundCount);
+			w.println(battleCount);
+
+			// PrintStreams don't throw IOExceptions during prints, they simply set a flag.... so check it here.
+			if (w.checkError()) {
+				out.println("I could not write the count!");
+			}
+		} catch (IOException e) {
+			out.println("IOException trying to write: ");
+			e.printStackTrace(out);
+		} finally {
+			if (w != null) {
+				w.close();
+			}
+		}
+		out.println("I have been a sitting duck for " + roundCount + " rounds, in " + battleCount + " battles.");
+	}
 }
