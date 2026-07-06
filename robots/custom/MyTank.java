b668a7d2f6550a19c696d788a0627b280aba45e9
CodeClash <player@codeclash.com>
2026-07-06 18:15:58 -0400

    Import droidpoet (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,24 @@
+// CodeClash ladder import
+// Source: https://github.com/PEZ/Bots/blob/HEAD/pez/femto/DroidPoet.java
+// Author: PEZ (Peter Strömberg) et al.   License: RWPCL
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+// By Peter Strmberg, http://robowiki.dyndns.org/?PEZ
+// Blind and strong
 
-import java.awt.*;
-
-public class MyTank extends Robot {
+public class MyTank extends Robot implements Droid {
     public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+        while (true) {
+            ahead(Math.random() * 600);
+            turnGunRight(Math.toDegrees(Math.atan2(getBattleFieldWidth() / 2 - getX(), getBattleFieldHeight() / 2 - getY())) -
+                getGunHeading());
+            fire(1.2);
         }
     }
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+    public void onHitWall(HitWallEvent e) {
+        turnRight(90 - getHeading() % 90);
     }
 }
