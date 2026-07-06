f32a35ba374e3d86aa6019769219d7cfc4bc26a0
CodeClash <player@codeclash.com>
2026-07-06 18:16:48 -0400

    Import myfirstkiller (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,58 @@
+// CodeClash ladder import
+// Source: https://github.com/andrekorol/my-robocode-robots/blob/HEAD/src/andykbr/MyFirstKiller.java
+// Author: andrekorol   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
+import java.awt.Color;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+/**
+ * MyFirstKiller - a robot by ()
+ */
+public class MyTank extends Robot
+{
+	/**
+	 * run: MyFirstKiller's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
 
-import java.awt.*;
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
+		 setColors(Color.red,Color.red,Color.red); // body,gun,radar
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}
 }
