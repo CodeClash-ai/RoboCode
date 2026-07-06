cf171355c8155e4906b472f5502c728389307223
CodeClash <player@codeclash.com>
2026-07-06 18:16:55 -0400

    Import oppswantmedead (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,66 @@
+// CodeClash ladder import
+// Source: https://github.com/andrekorol/my-robocode-robots/blob/HEAD/src/againstallodds/OppsWantMeDead.java
+// Author: andrekorol   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank (+ helper files flattened).
 package custom;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-
-import java.awt.*;
+import robocode.*;
+import java.awt.Color;
 
 public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
+	public void run() {
+		// Initialization of the robot should be put here
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		setBodyColor(Color.red);
+		setGunColor(Color.red);
+		setRadarColor(Color.red);
+		setScanColor(Color.red);
+		setBulletColor(Color.red);
+		
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);	
+	}
+	
+	/**
+	 * onWin:  Do a victory dance
+	 */
+	public void onWin(WinEvent e) {
+		for (int i = 0; i < 50; i++) {
+			turnRight(30);
+			turnLeft(30);
+		}
+	}
 }
