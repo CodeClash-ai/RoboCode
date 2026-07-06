a8dc7c62f09550c4d2e17d03fd288104944cc45b
CodeClash <player@codeclash.com>
2026-07-06 18:16:31 -0400

    Import jeujdapeu (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,44 @@
+// CodeClash ladder import
+// Source: https://github.com/joaomcarvalho/robocode/blob/HEAD/robots/jeuj/JeujDaPeu.java
+// Author: joaomcarvalho   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
+import java.awt.Color;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
 
-import java.awt.*;
+public class MyTank extends AdvancedRobot {
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
+public void run() {
+    setAdjustGunForRobotTurn(true);
+    setColors(Color.red,Color.blue,Color.green);
+
+    while (true) {
+        turnGunRight(20.30561967752975);
+        setAhead(339.8786182192478);
     }
+}
+public void onScannedRobot(ScannedRobotEvent e) {
+    setAhead(46.206349269139245);
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+    setTurnRight(62.02769513139049);
+
+    setTurnGunRight(370.38290095720924);
+
+    setTurnRadarRight(0.05010490467607376);
+
+    if (e.getDistance() < 60.123847177154005) {
+        setFire(13.353009945606784);
+    } else {
+        setFire(7.887119210483245);
     }
+}
+public void onHitByBullet(HitByBulletEvent e) {
+    setTurnRight(401.09876017910517);
+    setAhead(229.75500696032302 * -1);
+}
+public void onHitWall(HitWallEvent e) {
+    back(131.39765066471827);
+    setAhead(442.9924210980955 * -1);
+}
 }
