1daf2eea593e21055ff6c125360782ce69076305
CodeClash <player@codeclash.com>
2026-07-06 18:16:28 -0400

    Import ite_simple (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,82 @@
+// CodeClash ladder import
+// Source: https://github.com/it-economics/robocode/blob/HEAD/src/main/java/com/ite/robocode/SimpleBot.java
+// Author: it-economics   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 package custom;
 
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
+// IMPORTANT!!!
+// DO NOT CHANGE THIS CLASS ==> CREATE YOUR OWN ROBOT, you can copy this class for starters
 public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+	/**
+	 * run:  Fire's main run function
+	 */
+	@Override
+	public void run() {
+		// Set colors
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.black);
+
+		// This will be the default behaviour of your robot
+		while (true) {
+			ahead(100);
+		}
+	}
+
+	/**
+	 * onScannedRobot: if our robot sees another robot
+	 */
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+		turnLeft(15);
+		ahead(10);
+		// do something here
+	}
+
+	/**
+	 * onHitByBullet:  Ouch...our robot was hit by a bullet
+	 */
+	@Override
+	public void onHitByBullet(HitByBulletEvent e) {
+		// let's back off
+		turnLeft(15);
+		back(10);
+	}
+
+	/**
+	 * onHitRobot:  Yes, we hit a robot
+	 */
+	@Override
+	public void onHitRobot(HitRobotEvent e) {
+		// fire again
+		fire(1);
+	}
+
+	@Override
+	public void onHitWall(HitWallEvent event) {
+		super.onHitWall(event);
+		turnLeft(15);
+	}
+
+	@Override
+	public void scan() {
+		super.scan();
+	}
 }
