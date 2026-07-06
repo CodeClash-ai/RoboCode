2e7bef420dd3616281fbb47185aa2b5875192bec
CodeClash <player@codeclash.com>
2026-07-06 18:16:00 -0400

    Import exterminador (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,94 @@
+// CodeClash ladder import
+// Source: https://github.com/andrekorol/my-robocode-robots/blob/HEAD/src/univap/ExterminadorPrimeiroGrau.java
+// Author: andrekorol   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
 
+import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
-
+import robocode.WinEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 import java.awt.*;
 
 public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
+	static int count = 0;
+	double gunTurnAmt;
+	String trackName;
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+	public void run() {
+		// A morte não é o fim, é apenas um caminho que todos iremos tomar.
+		setBodyColor(Color.yellow);
+		setGunColor(Color.green);
+		setRadarColor(Color.pink);
+		setScanColor(Color.pink);
+		setBulletColor(Color.pink);
+
+		trackName = null;
+		setAdjustGunForRobotTurn(true);
+		gunTurnAmt = 10;
+
+		while (true) {
+			turnGunRight(gunTurnAmt);
+			count++;
+			if (count > 2) {
+				gunTurnAmt = -10;
+			}
+			if (count > 5) {
+				gunTurnAmt = 10;
+			}
+			if (count > 11) {
+				trackName = null;
+			}
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if (trackName != null && !e.getName().equals(trackName)) {
+			return;
+		}
+		if (trackName == null) {
+			trackName = e.getName();
+			out.println("Tracking " + trackName);
+		}
+		count = 0;
+		if (e.getDistance() > 150) {
+			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+			turnGunRight(gunTurnAmt);
+			turnRight(e.getBearing());
+			ahead(e.getDistance() - 140);
+			return;
+		}
+
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		turnGunRight(gunTurnAmt);
+		fire(3);
+
+		if (e.getDistance() < 100) {
+			if (e.getBearing() > -90 && e.getBearing() <= 90) {
+				back(40);
+			} else {
+				ahead(40);
+			}
+		}
+		scan();
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		if (trackName != null && !trackName.equals(e.getName())) {
+			out.println("Tracking " + e.getName() + " due to collision");
+		}
+		trackName = e.getName();
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		turnGunRight(gunTurnAmt);
+		fire(3);
+		back(50);
+	}
+
+	public void onWin(WinEvent e) {
+		for (int i = 0; i < 50; i++) {
+			turnRight(30);
+			turnLeft(30);
+		}
+	}
 }
