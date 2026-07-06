65b8eda16c5ce685c4a15ba2f9d5f070d90805c7
CodeClash <player@codeclash.com>
2026-07-06 18:17:49 -0400

    Import wallspoetas (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,25 @@
+// CodeClash ladder import
+// Source: https://github.com/PEZ/Bots/blob/HEAD/pez/femto/WallsPoetAS.java
+// Author: PEZ (Peter Strömberg) et al.   License: RWPCL
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-
-import java.awt.*;
+// WallsPoetAS, for the http://robowiki.net/?SpinBotChallenge
+// By Peter Stromberg, http://robowiki.net/?PEZ
+// Renamed WallsPoetAS -> MyTank.
 
 public class MyTank extends Robot {
     public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
+        turnGunLeft(Double.POSITIVE_INFINITY);
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        fire(3);
+        ahead(150);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        turnLeft(getHeading() % 90 - 90);
     }
 }
