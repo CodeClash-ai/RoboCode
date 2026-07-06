a2b39859ec8e50a6067faf853ccf839ef7a901fe
CodeClash <player@codeclash.com>
2026-07-06 18:16:19 -0400

    Import icarus (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,51 @@
+// CodeClash ladder import
+// Source: https://github.com/PEZ/Bots/blob/HEAD/pez/nano/Icarus.java
+// Author: PEZ (Peter Strömberg) et al.   License: RWPCL
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
+import robocode.*;
+import robocode.util.Utils;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+// This code is released under the RoboWiki Public Code Licence (RWPCL), datailed on:
+// http://robowiki.net/?RWPCL
+//
+// Icarus, by PEZ. - Something small
+// http://robowiki.net/?Icarus
+// $Id: Icarus.java,v 1.4 2004/08/25 16:51:40 peter Exp $
 
-import java.awt.*;
+public class MyTank extends AdvancedRobot {
+    static int direction = 7;
+    static double bulletVelocity = -3;
+    static double distance;
+    static int hits;
 
-public class MyTank extends Robot {
     public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
+	turnRadarRightRadians(Double.POSITIVE_INFINITY);
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+	distance = e.getDistance();
+	double v = 0.5 * bulletVelocity / distance;
+	if (hits > 3 && Math.random() > Math.pow(v, v)) {
+	    direction = -direction;
+	}
+	setAhead(direction * 15);
+	setTurnRight(e.getBearing() + 90 - direction);
+
+        double enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
+	setTurnGunRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getGunHeadingRadians() + e.getVelocity() * Math.sin(e.getHeadingRadians() - enemyAbsoluteBearing) / 13.0));
+	setFire(distance < 140 ? 3.0 : 1.9);
+
+	setTurnRadarLeftRadians(getRadarTurnRemaining());
+    }
+
+    public void onHitWall(HitWallEvent e) {
+	turnRight(e.getBearing() - 100);
+	ahead(100);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+	bulletVelocity = e.getVelocity();
+	hits++;
     }
 }
