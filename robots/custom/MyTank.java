3f731304d5833d8dfb7a673f037e8eec78e646b0
CodeClash <player@codeclash.com>
2026-07-06 18:17:33 -0400

    Import tirolio (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,34 @@
+// CodeClash ladder import
+// Source: https://github.com/barriosnahuel/algorithms-robocode-kenchu/blob/HEAD/src/main/java/pnt/TiroLioYCoshaGolda3.java
+// Author: barriosnahuel   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
 
-import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
-import java.awt.*;
+/**
+ * TODO : Javadoc for
+ * <p/>
+ * Created on 13/08/13, at 12:47.
+ *
+ * @author Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * @since 26.
+ */
+public class MyTank extends robocode.Robot {
 
-public class MyTank extends Robot {
+
+    @Override
     public void run() {
-        while(true) {
+
+        //noinspection InfiniteLoopStatement
+        while (true) {
             ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            turnRight(20);
         }
     }
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+//        fire(1);
     }
 }
