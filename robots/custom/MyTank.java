faeca6fba8dfe97b6213d03918c124dca2962fe9
CodeClash <player@codeclash.com>
2026-07-06 18:17:43 -0400

    Import vergere (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,27 @@
+// CodeClash ladder import
+// Source: https://github.com/John-Paul-R/Vergere/blob/HEAD/origin/nano/Vergere.java
+// Author: John-Paul-R   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
-
-import robocode.Robot;
+import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
-
-import java.awt.*;
-
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
-
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
-}
+import robocode.util.Utils;
+public class MyTank extends AdvancedRobot {
+	double ab,h;
+	public void run() {
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		turnRadarRightRadians(Double.POSITIVE_INFINITY);}
+	public void onScannedRobot(ScannedRobotEvent e) {
+		h = getHeadingRadians();
+        ab = e.getBearingRadians() + h;
+		setTurnRadarRightRadians(2.1 * Utils.normalRelativeAngle(ab - getRadarHeadingRadians()));
+		setTurnGunRightRadians(Utils.normalRelativeAngle(ab - getGunHeadingRadians()));
+	    setTurnRightRadians(ab+ Math.PI/2 -h);
+	    if (getDistanceRemaining() == 0)
+	    	setAhead(150 * Math.signum(Math.random()-.5));
+	    setFire(2);
+	    execute();
+	    }
+	}
