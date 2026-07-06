5ee8a9223971e9525c0d5e71e5a3d09bdb47fd43
CodeClash <player@codeclash.com>
2026-07-06 18:17:54 -0400

    Import wrecker (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,95 @@
+// CodeClash ladder import
+// Source: https://github.com/joaocarpim/robocode_CPS/blob/HEAD/Wrecker.java
+// Author: joaocarpim   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
-
+import robocode.*;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
-
 import java.awt.*;
+//import java.awt.Color;
 
-public class MyTank extends Robot {
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+// MyTank - A robot made by - José Roberto Lisboa da Silva Filho/ João Vitor Aparecido Carpim de Souza/ Luiz Gabriel Rodrigues Frei.
+
+/**
+ * MyTank - a robot by (your name here)
+ */
+public class MyTank extends AdvancedRobot {
+    int moveDirection=1;//Qual sentido se mexe.
+    /**
+     * Roda o MyTank.
+     */
     public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+        setColors( Color.white, Color.red, Color.white );
+        setScanColor(Color.white);
+        setBulletColor(Color.blue);
+        setAdjustGunForRobotTurn(true); // O canhão gira na posição oposta do radar.
+        turnRadarRightRadians(Double.POSITIVE_INFINITY);//Mantém o radar girando.
+         double v = 5;
+          double c = Math.PI*2;
+         double a = .1;
+         double b = .0053468;
+    
+          setMaxVelocity(v);
+          setAhead(100*999);
+          setTurnRight(360*999);
+          while(true)//Para se mover em curvas.
+          {
+              double t = getTime();
+              double f = a*Math.pow(Math.E,b*t);
+              double w = v/(c*f);       
+    
+              setMaxTurnRate(w);
+              execute();
+              System.out.println(t+"\t"+w);
+          }
+    }
+    
+    /**
+     * Ve o robo e vai atrás.
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double absBearing=e.getBearingRadians()+getHeadingRadians();//Comportamento/trajetória do inimigo.
+        double latVel=e.getVelocity() * Math.sin(e.getHeadingRadians() -absBearing);//Velocidade do inimigo.
+        double gunTurnAmt;//Quantidade de giro do canhão.
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//Leitura do radar focada no inimigo.
+        if(Math.random()>.9){
+            setMaxVelocity((12*Math.random())+12);//Muda a velocidade aleatoriamente durante a batalha.
+        }
+        if (e.getDistance() > 200) {//Se estiver há mais de 200.
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/22);//Quantidade de giro do canhão em relação ao inimigo.
+            setTurnGunRightRadians(gunTurnAmt); //Gira o canhão.
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//Vai em direção a futura posição (prevista) do inimigo.
+            setAhead((e.getDistance() - 140)*moveDirection);//Anda em frente.
+            setFire(2);//Atira.
+        }
+        else if (e.getDistance() > 150) {//Se estiver há mais de 150.
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/22);//Quantidade de giro do canhão em relação ao inimigo.
+            setTurnGunRightRadians(gunTurnAmt); //Gira o canhão.
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//Vai em direção a futura posição (prevista) do inimigo.
+            setAhead((e.getDistance() - 140)*moveDirection);//Anda em frente.
+            setFire(3);//Atira.
+        }
+        else{//Se estiver no alcance de menos de 150.
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/15);//Quantidade de giro do canhão em relação ao inimigo.
+            setTurnGunRightRadians(gunTurnAmt);//Gira o canhão.
+            setTurnLeft(-90-e.getBearing()); //Gira em volta do inimigo.
+            setAhead((e.getDistance() - 140)*moveDirection);//Anda em frente.
+            setFire(3);//fire
         }
     }
-
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+    public void onHitWall(HitWallEvent e){
+        moveDirection=-moveDirection;//Reverte a direção se acertar uma parede.
     }
-}
+    /**
+     * Se vencer faz a dança da vitória.
+     */
+    public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; i++) {
+            turnRight(30);
+            turnLeft(30);
+        }
+    }
+	}
