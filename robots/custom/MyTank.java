8e5a8c726f8545fba9c63f45eed64d5964f33436
CodeClash <player@codeclash.com>
2026-07-06 18:16:14 -0400

    Import hugbot (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,92 @@
+// CodeClash ladder import
+// Source: https://github.com/kylebennett/Robocode/blob/HEAD/BasicRobots/kylebennett/HugBot.java
+// Author: Kyle Bennett   License: unspecified
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
+/**
+ *
+ */
 package custom;
 
+import java.awt.Color;
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
 import robocode.Robot;
+import robocode.RobotDeathEvent;
 import robocode.ScannedRobotEvent;
 
-import java.awt.*;
-
+/**
+ * @author kylebennett
+ *
+ */
 public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+	private int turnDirection = 1;
+
+	private String currentTarget = null;
+
+	@Override
+	public void run() {
+
+		final Color purple = new Color(125, 60, 160);
+		setColors(purple, purple, purple);
+
+		while (true) {
+			turnRight(30 * turnDirection);
+		}
+	}
+
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+
+		final boolean theGruffalo = event.getName().contains("TheGruffalo")
+				&& getOthers() > 1;
+
+		if (currentTarget == null && !theGruffalo) {
+
+			currentTarget = event.getName();
+		}
+
+		if (currentTarget.equals(event.getName())) {
+
+			turnDirection = event.getBearing() >= 0 ? 1 : -1;
+			turnRight(event.getBearing() * turnDirection);
+
+			ahead(event.getDistance() + 10);
+
+			scan();
+		}
+	}
+
+	@Override
+	public void onHitByBullet(HitByBulletEvent event) {
+
+	}
+
+	@Override
+	public void onHitRobot(HitRobotEvent event) {
+
+		final boolean theGruffalo = event.getName().contains("TheGruffalo")
+				&& getOthers() > 1;
+
+		if (!theGruffalo) {
+			turnDirection = event.getBearing() >= 0 ? 1 : -1;
+			turnRight(event.getBearing());
+			fire(3);
+		}
+	}
+
+	@Override
+	public void onHitWall(HitWallEvent event) {
+
+	}
+
+	@Override
+	public void onRobotDeath(RobotDeathEvent event) {
+
+		if (event.getName().equals(currentTarget)) {
+			currentTarget = null;
+		}
+	}
 }
