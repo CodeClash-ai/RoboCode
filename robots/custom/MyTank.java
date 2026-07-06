168560fc56c42cd2d56f617a8f08e2d025333bde
CodeClash <player@codeclash.com>
2026-07-06 18:16:02 -0400

    Import fire (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,89 @@
+// CodeClash ladder import
+// Source: https://github.com/robo-code/robocode/blob/HEAD/robocode.samples/src/main/java/sample/Fire.java
+// Author: Robocode (Mathew Nelson / Flemming N. Larsen et al.)   License: EPL-1.0
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
+/*
+ * Copyright (c) 2001-2025 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 package custom;
 
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+
+/**
+ * Fire - a sample robot that demonstrates basic targeting and reactionary movement.
+ * <p>
+ * This robot sits still, continuously rotates its gun, and only moves when hit.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ */
+public class MyTank extends Robot {
+	int dist = 50; // Distance to move when hit by a bullet
+
+	/**
+	 * Main robot logic that sets colors and rotates the gun continuously
+	 */
+	public void run() {
+		// Set colors
+		setBodyColor(Color.orange);
+		setGunColor(Color.orange);
+		setRadarColor(Color.red);
+		setScanColor(Color.red);
+		setBulletColor(Color.red);
+
+		// Spin the gun around slowly... forever
+		while (true) {
+			turnGunRight(5);
+		}
+	}
+
+	/**
+	 * Fires at detected robots with variable power based on distance and energy
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// If the other robot is close by, and we have plenty of life,
+		// fire hard!
+		if (e.getDistance() < 50 && getEnergy() > 50) {
+			fire(3);
+		} // otherwise, fire 1.
+		else {
+			fire(1);
+		}
+		// Call scan again, before we turn the gun
+		scan();
+	}
+
+	/**
+	 * Reacts to being hit by turning perpendicular to the incoming bullet and moving
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
+
+		ahead(dist);
+		dist *= -1;
+		scan();
+	}
+
+	/**
+	 * Targets and fires at robots that collide with this robot
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+
+		turnGunRight(turnGunAmt);
+		fire(3);
+	}
 }
