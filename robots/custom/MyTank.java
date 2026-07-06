59799481b2ea52ccfb28a5009c72f035f327c941
CodeClash <player@codeclash.com>
2026-07-06 18:17:42 -0400

    Import velocirobot (RoboCode ladder)

--- robots/custom/MyTank.java
+++ robots/custom/MyTank.java
@@ -1,21 +1,58 @@
+// CodeClash ladder import
+// Source: https://github.com/robo-code/robocode/blob/HEAD/robocode.samples/src/main/java/sample/VelociRobot.java
+// Author: Robocode (Mathew Nelson / Flemming N. Larsen et al.)   License: EPL-1.0
+// Imported verbatim; only repackaged to the arena package + main class renamed to MyTank.
 package custom;
 
-import robocode.Robot;
+
+import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
+import robocode.RateControlRobot;
 import robocode.ScannedRobotEvent;
 
-import java.awt.*;
 
-public class MyTank extends Robot {
-    public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
+/**
+ * VelociRobot - a sample robot that demonstrates rate-controlled movement patterns.
+ * This robot uses the RateControlRobot class to alternate between forward and backward
+ * movement while responding to events.
+ *
+ * @author Joshua Galecki (original)
+ */
+public class MyTank extends RateControlRobot {
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+	int turnCounter;
+	public void run() {
+
+		turnCounter = 0;
+		setGunRotationRate(15);
+
+		while (true) {
+			if (turnCounter % 64 == 0) {
+				// Straighten out if we were hit by a bullet and are turning
+				setTurnRate(0);
+				// Go forward with a velocity of 4
+				setVelocityRate(4);
+			}
+			if (turnCounter % 64 == 32) {
+				// Go backwards, faster
+				setVelocityRate(-6);
+			}
+			turnCounter++;
+			execute();
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Turn to confuse the other robot
+		setTurnRate(5);
+	}
+
+	public void onHitWall(HitWallEvent e) {
+		// Move away from the wall
+		setVelocityRate(-1 * getVelocityRate());
+	}
 }
